f9a1126a2b07626e46ff35550997bd5a46c50d67
caua-carvalho <caua.carma033@gmail.com>
2025-04-07 01:24:48 -0300

    update

--- conexao.php
+++ conexao.php
@@ -9,6 +9,4 @@ $conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo '<script> alert("Conexão com o banco de dados estabelecida com sucesso!"); </script>';
 ?>

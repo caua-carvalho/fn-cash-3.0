e4e010526eb6b8c840376b850d782af9d92ac5b1
caua-carvalho <caua.carma033@gmail.com>
2025-04-22 22:12:43 -0300

    ajuste na sidebar

--- pages/footer.php
+++ pages/footer.php
@@ -1,7 +1,4 @@
     </div>
 </div>
-<footer class="footer text-gray text-center py-2">
-    <p>&copy; 2025 FN Cash. All rights reserved.</p>
-</footer>
 <script src="../JavaScript/sidebar.js"></script>
 </body>
--- pages/nav.php
+++ pages/nav.php
@@ -1,17 +1,51 @@
-
 <div class="wrapper">
-  <nav id="sidebar">
-    <button class="toggle-btn" id="toggleSidebar">☰</button>
-    <ul>
-      <li><i class="bi bi-house-door"></i><span>Home</span></li>
-      <li><i class="bi bi-plug"></i><span>Plugins</span></li>
-      <li><i class="bi bi-people"></i><span>Usuários</span></li>
-      <li><i class="bi bi-cash-coin"></i><span>Transações</span></li>
-      <li><i class="bi bi-tags"></i><span>Categoria</span></li>
-      <li><i class="bi bi-wallet2"></i><span>Conta</span></li>
-      <li><i class="bi bi-graph-up"></i><span>Orçamento</span></li>
-    </ul>
-  </nav>
+    <nav id="sidebar">
+        <button class="toggle-btn" id="toggleSidebar">☰</button>
+        <ul>
+            <li>
+                <a href="categorias.php" class="text-white icone-sidebar">
+                    <i class="bi bi-house-door fs-10"></i>
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="plugins.php" class="text-white icone-sidebar">
+                    <i class="bi bi-plug fs-4"></i>
+                    <span>Plugins</span>
+                </a>
+            </li>
+            <li>
+                <a href="usuarios.php" class="text-white icone-sidebar">
+                    <i class="bi bi-people fs-6"></i>
+                    <span>Usuários</span>
+                </a>
+            </li>
+            <li>
+                <a href="transacoes.php" class="text-white icone-sidebar">
+                    <i class="bi bi-cash-coin"></i>
+                    <span>Transações</span>
+                </a>
+            </li>
+            <li>
+                <a href="categoria.php" class="text-white icone-sidebar">
+                    <i class="bi bi-tags"></i>
+                    <span>Categoria</span>
+                </a>
+            </li>
+            <li>
+                <a href="conta.php" class="text-white icone-sidebar">
+                    <i class="bi bi-wallet2"></i>
+                    <span>Conta</span>
+                </a>
+            </li>
+            <li>
+                <a href="orcamento.php" class="text-white icone-sidebar">
+                    <i class="bi bi-graph-up"></i>
+                    <span>Orçamento</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
 
-  <div id="main-content">
+    <div id="main-content">
     

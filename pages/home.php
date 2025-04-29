05973eb55def6dc70fb9057a5421ff9cc38c37e0
Lucas Pereira <caua.carma033@gmail.com>
2025-04-29 20:37:23 -0300

    ajuste na sidebar e home

--- pages/home.php
+++ pages/home.php
@@ -3,8 +3,16 @@ require_once 'header.php';
 
 require_once 'sidebar.php';
 ?>
+<style>
+    .logo-home {
+        opacity: 20%;
+        transition: all 0.2s ease-in-out;
+    }
+</style>
 
-
+<div class="h-100 d-flex justify-content-center align-items-center">
+    <img src="../logo/logo_escrita.svg" class="w-50 logo-home" id="logo-home">
+</div>
 <?php
 require_once 'footer.php';
 ?>

4ecd4c9117e4a658024829d24dc4c88b4cedb1bb
caua-carvalho <caua.carma033@gmail.com>
2025-05-02 16:56:42 -0300

    exclusao de links para o css do boostrap

--- pages/header.php
+++ pages/header.php
@@ -14,8 +14,6 @@ if (!isset($_SESSION['id_usuario'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" href="../logo/icone.svg">
 	<title>Fn-Cash</title>
-	<!-- CDN css bootstrap -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
 	<!-- CDN script bootstrap -->
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>

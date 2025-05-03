eb3e03fd165c9baecd4eacb1f7bbadd18e9cb2fb
caua-carvalho <caua.carma033@gmail.com>
2025-05-02 21:04:08 -0300

    juste na logo do home.php

--- pages/home.php
+++ pages/home.php
@@ -7,10 +7,18 @@ require_once 'sidebar.php';
     .logo-home {
         opacity: 20%;
         transition: all 0.2s ease-in-out;
+        width: 50%;
+    }
+
+    .container_logo_home {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
     }
 </style>
 
-<div class="h-100 d-flex justify-content-center align-items-center">
+<div class="container_logo_home">
     <img src="../logo/logo_escrita.svg" class="w-50 logo-home" id="logo-home">
 </div>
 <?php

7b5a48f1ae98d109e0f05776d1d125d779c2d6d2
caua-carvalho <caua.carma033@gmail.com>
2025-05-09 23:01:53 -0300

    ajuste na font, agora esta aplicando montserrat para todas as paginas

--- pages/header.php
+++ pages/header.php
@@ -29,6 +29,11 @@ if (!isset($_SESSION['id_usuario'])) {
     <!-- CSS CUSTOM -->
     <link rel="stylesheet" href="../css/style.css">
 	<link rel="stylesheet" href="sidebar/sidebar.css">
+
+	<!-- GOOGLE FONTS MONTSERRAT -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 	
 </head>
 <body class="dark-theme">
